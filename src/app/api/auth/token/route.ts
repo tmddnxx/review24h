3e6749b9c1c9ca9e19a673ee211260572f37d742
tmddnxx <lsw6878@naver.com>
2024-08-13 20:37:00 +0900

    fix: refreshToken을 redis 저장방식 추가

--- src/app/api/auth/token/route.ts
+++ src/app/api/auth/token/route.ts
@@ -3,6 +3,7 @@ import { signJwtAccessToken, verifyJwt } from "@/utils/auth/jwt";
 import { jwtDecode } from "jwt-decode";
 import { cookies } from "next/headers";
 import { getUserByMno } from "@/services/AuthService";
+import redis from "../../../../../redis/redis";
 
 
 export async function POST(req:NextRequest) {
@@ -14,6 +15,12 @@ export async function POST(req:NextRequest) {
     }
 
     const verifyAT = verifyJwt(accessToken);
+    const at_ex = jwtDecode(accessToken).exp;
+    const currentTime = Math.floor(Date.now() / 1000 ); 
+
+    if(at_ex-currentTime <= 0){ // 액세스토큰 만료된 경우
+        return new NextResponse(JSON.stringify({message: "액세스토큰이 만료되었습니다."}), {status : 401})
+    }
 
     if(!verifyAT){
         // 엑세스토큰이 유효하지 않은 경우
@@ -24,32 +31,47 @@ export async function POST(req:NextRequest) {
     const refreshToken = cookie.get('refreshToken');
     const refreshTokenString = (refreshToken as { value: string })?.value || '';
 
-    console.log(refreshToken?.value);
+    const rt_ex = jwtDecode(refreshTokenString).exp;
+
+    if((rt_ex-currentTime) <= 0){
+        // 쿠키 리프레시토큰이 만료된 경우
+        return new NextResponse(JSON.stringify({message: "리프레시토큰 만료되었습니다."}), {status : 401})
+    }
+        
 
     const verifyRT = verifyJwt(refreshTokenString);
    
     if(!verifyRT){
-        // 리프레시토큰이 유효하지 않은 경우
-        return new NextResponse(JSON.stringify({message: "리프레시토큰이 유효하지 않습니다."}),{status: 401});
+        // 쿠키 리프레시토큰이 유효하지 않은 경우
+        return new NextResponse(JSON.stringify({message: "리프레시토큰이 유효하지 않습니다."}),{status: 401})
     }
     
-    const rt_ex = jwtDecode(refreshTokenString).exp;
-    const currentTime = Math.floor(Date.now() / 1000 ); 
-
-    if((rt_ex-currentTime) <= 0){
-        // 리프레시토큰이 만료된 경우
-        return new NextResponse(JSON.stringify({message: "리프레시토큰 만료되었습니다."}), {status : 403})
+    // 레디스 토큰 확인
+    const redisBlackRT = await redis.get(`blackList:${refreshToken?.value}`);
+    if(redisBlackRT === 'blackList'){
+        // 블랙리스트에 등록된 토큰인지 확인
+        return new NextResponse(JSON.stringify({message: "블랙리스트에 등록된 토큰입니다."}),{status: 403})
     }
-        
+
     const at_mno = jwtDecode(accessToken).mno;
     const rt_mno = jwtDecode(refreshTokenString).mno;
 
-    if(at_mno !== rt_mno){
-        // 액세스토큰의 mno와 리프레시토큰의 mno가 동일하지 않은 경우
-        return new NextResponse(JSON.stringify({message: "사용자가 일치하지 않습니다."}), {status: 403})
+    const redisRT = await redis.get(`refreshToken:mno:${rt_mno}`);
+
+    if(redisRT !== refreshTokenString){
+        // Redis 토큰과 쿠키토큰 미 일치 경우
+        return new NextResponse(JSON.stringify({message: "토큰이 일치하지 않습니다."}), {status:401})
     }
-        
-    const user = await getUserByMno(rt_mno);
+    
+    const decodeRedisRT = jwtDecode(redisRT);
+
+    if(at_mno !== decodeRedisRT.mno){
+        // 액세스토큰의 mno와 리프레시토큰의 mno가 동일하지 않은 경우
+        return new NextResponse(JSON.stringify({message: "사용자가 일치하지 않습니다."}), {status: 401})
+    }
+    
+    
+    const user = await getUserByMno(decodeRedisRT.mno);
     const newAccessToken = signJwtAccessToken({mno: user.mno});
 
     return NextResponse.json({accessToken: newAccessToken})
