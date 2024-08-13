5ff35d3a2fb39850113dd194dd7363cdc6666842
tmddnxx <lsw6878@naver.com>
2024-08-13 20:38:14 +0900

    feat: 로그아웃 시 redis에 refreshToken 블랙리스트 등록

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,9 +1,14 @@
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import redis from "../../../../../redis/redis";
 
-export async function POST(req:Request) {
+export async function POST(req:NextRequest) {
     const cookie = cookies();
-    
+    const refreshToken = cookie.get('refreshToken')?.value;
+
+    redis.set(`blackList:${refreshToken}`, 'blackList'); // 로그아웃시 발금된 refreshToken 블랙리스트 등록
+
     cookie.delete('refreshToken');
+
     return new NextResponse(JSON.stringify({message: "로그아웃"}))
 }
