1a43cd0f9f5cd85d4645c59fe732eb283d59c15d
tmddnxx <lsw6878@naver.com>
2024-08-16 16:18:55 +0900

    fix: 회원 관련 DB접근 서비스로직 수정

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,13 +1,15 @@
 // 회원가입 로그인 관련 서비스
 import prisma from "../../prisma/client";
 import * as bcrypt from 'bcrypt';
+import CryptoJS from "crypto-js";
 
 // 회원가입
 export async function signUp(body:any){
     try {
         const hashedPW = await bcrypt.hash(body.password, 10); // 10 => 해시반복횟수
-        const hashedBID = await bcrypt.hash(body.baminID, 10);
+        const hashedBID = CryptoJS.AES.encrypt(body.baminID, process.env.NEXTAUTH_SECRET).toString()
         const hashedBPW = await bcrypt.hash(body.baminPW, 10); // 10 => 해시반복횟수
+        
         const result = await prisma.$transaction(async (prisma) => {
             const user = await prisma.user.create({
                 data: {
@@ -15,7 +17,7 @@ export async function signUp(body:any){
                     password: hashedPW, 
                     name: body.name,
                     phone: body.phone,
-                    social: body.social,
+                    provider: body.provider,
                 }
             });
 
@@ -26,6 +28,15 @@ export async function signUp(body:any){
                     baminPW: hashedBPW,
                 }
             });
+
+            const social = await prisma.social.create({
+                data: {
+                    mno: user.mno,
+                    kakao_Id: body.provider === 'kakao' ? body.socialID : '',
+                    naver_Id: body.provider === 'naver' ? body.socialID : ''
+                }
+            });
+
         });
 
         return true;
@@ -74,8 +85,8 @@ export async function getUserByMno(mno:number){
         }
     })
     if(user !== null){
-        const {password, ...userWithoutPass} = user;
-        return userWithoutPass;
+        
+        return user;
     }else{
         return null;
     }
@@ -112,4 +123,87 @@ export async function getUserByPhone(phone:string){
     }else{
         return null;
     }
+}
+
+// 소셜 아이디로 유저찾기
+export async function getUserBySocialID(provider:string ,socialID:string){
+    
+    const user = await prisma.user.findFirst({
+        where: {
+            social: {
+                ...(provider === 'kakao' ? { kakao_Id: socialID } : {}),
+                ...(provider === 'naver' ? { naver_Id: socialID } : {}),
+            },
+        },
+        include: {
+            social: true,
+        },
+    });
+    
+    if(!user){
+        return null
+    }
+
+    return user;
+}
+
+// User + Bamin 테이블 조인
+export async function getUserAndBaminByMno(mno:number){
+
+    const result = await prisma.user.findUnique({
+        where: {
+            mno:mno,
+        },
+        include: {
+            bamin_account: true,
+        }
+    })
+
+    if(!result){
+        return null;
+    }
+
+    return result;
+}
+
+// User 정보 업데이트
+export async function updateUserByMno(body:any){
+    await prisma.user.update({
+        where: {
+            mno: body.mno,
+        },
+        data: {
+            email: body.email,
+            name: body.name,
+            phone: body.phone,
+        }
+    })
+}
+
+// 소셜ID 연동
+export async function updateSocialID(mno:number, social:string, socialID: string){
+    
+      // 데이터 업데이트에 사용할 객체를 생성합니다.
+      let updateData: any = {};
+
+      // provider에 따라 업데이트할 필드를 설정합니다.
+      if (social === 'kakao') {
+          updateData = {
+              kakao_Id: socialID,
+          };
+      } else if (social === 'naver') {
+          updateData = {
+              naver_Id: socialID,
+          };
+      }
+  
+      // 소셜ID 업데이트
+      await prisma.social.update({
+          where: {
+              mno: mno, // 업데이트할 레코드의 식별자로 mno를 사용합니다.
+          },
+          data: {
+              ...updateData, // 업데이트할 데이터를 스프레드 연산자로 추가합니다.
+          },
+      });
 }
