84b42e19c9f442e80573e6a891460e5f22963f3a
tmddnxx <lsw6878@naver.com>
2024-08-16 16:16:51 +0900

    fix : 헤더 액세스토큰 전송방식 변경 : body -> headers

--- src/app/(DashboardLayout)/layout/header/Header.tsx
+++ src/app/(DashboardLayout)/layout/header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, AppBar, Toolbar, styled, Stack, IconButton, Badge, Button } from '@mui/material';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
@@ -7,6 +7,7 @@ import Profile from './Profile';
 import { IconBellRinging, IconMenu } from '@tabler/icons-react';
 import { signOut, useSession } from 'next-auth/react';
 import { jwtDecode } from 'jwt-decode';
+import Logo from '../shared/logo/Logo';
 
 interface ItemType {
   toggleMobileSidebar:  (event: React.MouseEvent<HTMLElement>) => void;
@@ -19,64 +20,62 @@ const Header = ({toggleMobileSidebar}: ItemType) => {
 
   // 액세스토큰 로컬 스토리지 저장 및 자동갱신 
   const session = useSession();
-  
-  if(session.status === 'authenticated'){
-    const checkTokenExpiry = async () => {
-      const accessToken = session.data.user.accessToken;
+  const [isLogin, setIsLogin] = useState(false); // 로그인 상태확인
 
-      // 액세스 토큰이 존재하면 로컬 스토리지에 저장
-      if (accessToken) {
-      localStorage.setItem('accessToken', accessToken);
-      }
-      
-      let LSAT = localStorage.getItem('accessToken');
-      if (LSAT) {
+  useEffect(() => {
+    if (session.status === 'authenticated') {
+      setIsLogin(true);
+
+      const checkTokenExpiry = async () => {
+        const accessToken = session.data.user.accessToken;
+
+        if (accessToken) {
+          localStorage.setItem('accessToken', accessToken);
+        }
+
+        let LSAT = localStorage.getItem('accessToken');
+        if (LSAT) {
           const decoded = jwtDecode(LSAT);
-          const at_ex = decoded.exp; // 액세스 토큰 만료 시간
-          const currentTime = Math.floor(Date.now() / 1000); // 현재 시간
+          const at_ex = decoded.exp;
+          const currentTime = Math.floor(Date.now() / 1000);
 
-          // 액세스 토큰 만료 5분 전인지 확인
-          if ((at_ex - currentTime) <= 5 * 60) {
-              console.log("만료 5분 전");
+          if ((at_ex - currentTime) <=  5 * 60) {
+            try {
+              const response = await fetch('/api/auth/token', {
+                method: 'POST',
+                headers: { Authorization: LSAT },
+              });
 
-              try {
-                  const response = await fetch('/api/auth/token', {
-                      method: 'POST',
-                      headers: {'Content-Type': 'application/json'},
-                      body: JSON.stringify({ accessToken: LSAT }),
-                      credentials: 'include',
-                  });
-                  if(!response.ok){
-                      const errorData = await response.json();
-                      console.error(errorData);
+              if (!response.ok) {
+                const errorData = await response.json();
+                alert(errorData.message);
 
-                      handleLogout();
-                      return;
-                  }
-                  const data = await response.json();
+                handleLogout();
+                return;
+              }
 
-                  if (data.accessToken) {
-                      localStorage.setItem('accessToken', data.accessToken);
-                      console.log('새 액세스 토큰:', data.accessToken);
+              const data = await response.json();
 
-                      // 새 액세스 토큰으로 갱신된 시간 확인
-                      LSAT = data.accessToken; // 갱신된 액세스 토큰으로 업데이트
-                      
-                      session.update({accessToken: data.accessToken});
-                      
-                  } else {
-                      console.log("새로운 토큰을 발급 받을 수 없습니다.");
-                  }
-              } catch (error) {
-                  console.error('토큰 갱신 오류:', error);
-                  handleLogout();
-              };
-            };
-          };
-    };
+              if (data.accessToken) {
+                localStorage.setItem('accessToken', data.accessToken);
+                console.log('새 액세스 토큰:', data.accessToken);
 
-    checkTokenExpiry();
-  }
+                LSAT = data.accessToken;
+                session.update({ accessToken: data.accessToken });
+              } else {
+                console.log("새로운 토큰을 발급 받을 수 없습니다.");
+              }
+            } catch (error) {
+              console.error('토큰 갱신 오류:', error);
+              handleLogout();
+            }
+          }
+        }
+      };
+
+      checkTokenExpiry();
+    }
+  }, [session]); // session이 변경될 때만 실행
 
   const handleLogout = () => {
     localStorage.removeItem('accessToken');
@@ -85,6 +84,7 @@ const Header = ({toggleMobileSidebar}: ItemType) => {
         method:'POST',
     })
     signOut();
+    setIsLogin(false); 
   };
 
   const AppBarStyled = styled(AppBar)(({ theme }) => ({
@@ -104,6 +104,7 @@ const Header = ({toggleMobileSidebar}: ItemType) => {
   return (
     <AppBarStyled position="sticky" color="default">
       <ToolbarStyled>
+
         <IconButton
           color="inherit"
           aria-label="menu"
@@ -118,7 +119,8 @@ const Header = ({toggleMobileSidebar}: ItemType) => {
           <IconMenu width="20" height="20" />
         </IconButton>
 
-
+        <Logo/>
+        
         <IconButton
           size="large"
           aria-label="show 11 new notifications"
@@ -129,16 +131,20 @@ const Header = ({toggleMobileSidebar}: ItemType) => {
           <Badge variant="dot" color="primary">
             <IconBellRinging size="21" stroke="1.5" />
           </Badge>
-
         </IconButton>
+
         <Box flexGrow={1} />
         <Stack spacing={1} direction="row" alignItems="center">
-          <Button variant="contained" component={Link} href="/authentication/login"   disableElevation color="primary" >
-            Login
-          </Button>
-          <Button variant="contained" onClick={handleLogout} disableElevation color="primary" >
-            LogOut
-          </Button>
+          {!isLogin && (
+            <Button variant="contained" component={Link} href="/authentication/login"   disableElevation color="primary" >
+              Login
+            </Button> 
+          )}
+          {isLogin && (
+            <Button variant="contained" onClick={handleLogout} disableElevation color="primary" >
+              LogOut
+            </Button>
+          )}
           <Profile />
         </Stack>
       </ToolbarStyled>
