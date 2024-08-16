98d002dc7850e149c0278b7d3fb7b3fe39c58957
tmddnxx <lsw6878@naver.com>
2024-08-16 16:20:50 +0900

    fix: 회원가입 요청버튼 삭제 및 이전

--- src/app/authentication/auth/AuthRegister3.tsx
+++ src/app/authentication/auth/AuthRegister3.tsx
@@ -10,31 +10,6 @@ import useBaminPW from '../hooks/step3/useBaminPW';
 
 const AuthRegister3 = ({ subtitle, subtext, handleChange, handleNext, handlePrev, formData }: RegisterFormProps ) => {
     
-    
-    // const handleSubmit = async () => {
-    //     try {
-    //       // 서버에 POST 요청 보내기
-    //       const response = await fetch('/api/auth/signUp', {
-    //           method: 'POST',
-    //           headers: {
-    //               'Content-Type': 'application/json',
-    //           },
-    //           body: JSON.stringify(formData),
-    //       })
-    //       const data = await response.json();
-    //       if(!response.ok){
-    //         alert('중복오류가 발생했습니다. 다시 시도해주세요.');
-    //       }else{
-    //        window.location.href= '/authentication/login';
-    //       }
-    //     } catch (error) {
-    //       // 에러가 발생한 경우
-    //       console.error('Registration error:', error);
-    //       // 사용자에게 에러 메시지를 표시하거나, 추가적인 에러 처리를 할 수 있습니다.
-    //       alert('회원가입 중 문제가 발생했습니다. 다시 시도해 주세요.');
-    //     }
-    //   };
-
     const {
         baminIDChange,
         isBaminDuplicateCheck, // 중복검사 여부 true = 통과
