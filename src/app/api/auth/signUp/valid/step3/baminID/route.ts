6c4b831e73be8986bd4e62675495206a412a5bfd
tmddnxx <lsw6878@naver.com>
2024-08-20 18:15:36 +0900

    fix: console.log 코드추가

--- src/app/api/auth/signUp/valid/step3/baminID/route.ts
+++ src/app/api/auth/signUp/valid/step3/baminID/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req:NextRequest){
     const body = await req.json();
     const {baminID} = body;
-    
+    console.log("배민아이디 : ", baminID)
     // 1. 데이터 존재 확인
     if(!baminID){
         return NextResponse.json({message: '아이디를 입력해주세요.', success: false});
@@ -12,7 +12,9 @@ export async function POST(req:NextRequest){
 
     // 2. 중복확인
     const bamin_account = await getBaminUserByID(baminID);
+    console.log("배민 어카운트 : ",bamin_account)
     if(bamin_account !== null){
+        
         return NextResponse.json({message: '이미 존재하는 계정입니다.', success: false});
     }
 
